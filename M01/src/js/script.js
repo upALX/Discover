69a30197256e3ee33006878b86a0f380e022fc1c
Alexandre <alexandreaddres@gmail.com>
2023-09-25 07:11:45 -0300

    fixing bugs css

--- M01/src/js/script.js
+++ M01/src/js/script.js
@@ -5,7 +5,7 @@ function toogleModel(){
     const imgHtml = document.querySelector(".profile img")
 
     if (html.classList.contains('light-mode')){
-        imgHtml.setAttribute('src', 'https://th.bing.com/th?id=OIP.YjJSBQVO5Cy9RBxwNqfj7AHaJ5&w=216&h=289&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2')
+        imgHtml.setAttribute('src', './assets/alx-relax.jpeg')
     }else{
         imgHtml.setAttribute('src', "https://avatars.githubusercontent.com/u/59508016?v=4")
     }
